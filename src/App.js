ac1eada15effa5fcff780c2c621f5a3fd2fea140
shrikanthg98 <shrikanthg18@gmail.com>
2022-08-29 21:32:50 +0530

    minor fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useContext } from 'react';
+import React, { useContext } from 'react';
 import Toggle from './components/toggle/Toggle';
 import Intro from "./components/intro/Intro";
 import About from "./components/about/About";
